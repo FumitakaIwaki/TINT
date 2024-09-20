c7df5eeb9079e54587ebe578e10e1c181591c564
FumitakaIwaki <iwaki2323a@gmail.com>
2024-09-20 13:31:58 +0900

    get_source_triangleのテストを追加

--- tint_prj/test/test_add_identity.jl
+++ tint_prj/test/test_add_identity.jl
@@ -1,10 +1,13 @@
 using Test
+using DataFrames
 using Graphs
 using SimpleWeightedGraphs
+using Combinatorics
 
 include("../src/category_builder.jl")
-using .CategoryBuilder: add_identity
+using .CategoryBuilder: build, add_identity, get_source_triangle
 
+# add_identity関数のテストセット
 @testset "add_identity" begin
     graph = SimpleWeightedDiGraph(5)
     graph = add_identity(graph)
@@ -16,4 +19,31 @@ using .CategoryBuilder: add_identity
     for edge in edges(graph)
         @test edge.src == edge.dst
     end
+end
+
+# get_source_triangleのテストセット
+@testset "get_source_triangle" begin
+    # 完全グラフの時のテスト
+    potential_category = SimpleWeightedDiGraph(3)
+    for (dom, cod) in permutations([i for i in 1:3], 2)
+        add_edge!(potential_category, dom, cod, 0.5)
+    end
+    data = [
+        1 0.5;
+        2 0.725;
+        3 0.275;
+    ]
+    source_init_images = DataFrame(data, [:to, :weight])
+    source_init_images.to = Int64.(source_init_images.to)
+
+    triangle = get_source_triangle(source_init_images, potential_category)
+    @test triangle[1] == 1
+    @test triangle[2] == 2
+
+    # 辺がないグラフの時のテスト
+    potential_category = SimpleWeightedDiGraph(3)
+    triangle = get_source_triangle(source_init_images, potential_category)
+
+    @test triangle[1] == 0
+    @test triangle[2] == 0
 end
