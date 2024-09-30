ed6de3d2d3d9359f6d172b90470cb1d9660a4478
FumitakaIwaki <iwaki2323a@gmail.com>
2024-09-30 16:25:23 +0900

    bmf_builderの修正とテストの追加

--- tint_prj/src/bmf_builder.jl
+++ tint_prj/src/bmf_builder.jl
@@ -10,35 +10,38 @@ using .FunctorBuilder
 function build(source::Int, target::Int,
     source_category::SimpleWeightedDiGraph, target_category::SimpleWeightedDiGraph
     )#::(SimpleWeightedDiGraph, Functor)
+    _target_category = copy(target_category)
 
     objects = Dict{Int, Int}()
     morphisms = Dict{Tuple, Tuple}()
     objects[source] = target
 
     for source_image in vertices(source_category)
-        add_edge!(target_category, target, source_image, 1.0)
+        add_edge!(_target_category, target, source_image, 1.0)
         morphisms[(source, source_image)] = (target, source_image)
     end
 
-    return (target_category, Functor(objects, morphisms))
+    return (_target_category, Functor(objects, morphisms))
 end
 
 # BMFを構築する関数（構造考慮）
 function build(source::Int, target::Int,
     source_triangle_images::Vector{Int}, target_category::SimpleWeightedDiGraph
     )#::(SimpleWeightedDiGraph, Functor)
+    _target_category = copy(target_category)
 
     objects = Dict{Int, Int}()
     morphisms = Dict{Tuple, Tuple}()
+    objects[source] = target
     triangle_dom, triangle_cod = source_triangle_images
-    add_edge!(target_category, target, triangle_dom)
-    add_edge!(target_category, target, triangle_cod)
-    add_edge!(target_category, triangle_dom, triangle_cod)
+    add_edge!(_target_category, target, triangle_dom)
+    add_edge!(_target_category, target, triangle_cod)
+    add_edge!(_target_category, triangle_dom, triangle_cod)
     morphisms[(source, triangle_dom)] = (target, triangle_dom)
     morphisms[(source, triangle_cod)] = (target, triangle_cod)
     morphisms[(triangle_dom, triangle_cod)] = (triangle_dom, triangle_cod)
     
-    return (target_category, Functor(objects, morphisms))
+    return (_target_category, Functor(objects, morphisms))
 end
 
 end # BMFBuilder
