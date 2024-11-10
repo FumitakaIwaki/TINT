17206e21204172f586aeb4413be66769aba37a11
FumitakaIwaki <iwaki2323a@gmail.com>
2024-11-10 12:44:49 +0900

    シミュレーション実行ファイル修正

--- tint_prj/run_simulation.jl
+++ tint_prj/run_simulation.jl
@@ -3,22 +3,48 @@ using .TINT
 
 using Random
 
-function simulation()
+function run_object()
     config = Dict{String, Any}(
-    "metaphor_set" => Set([("蝶", "踊り子"), ("笑顔", "花"), ("粉雪", "羽毛")]),
-    "assoc_file" => "data/three_metaphor_assoc_data.csv",
-    "image_file" => "data/three_metaphor_images.csv",
-    "out_dir" => "tint_prj/out",
-    "NN" => 0,
-    "steps" => 1,
-    "search_method" => "deterministic",
-    "seed" => 1234,
-    "verbose" => false,
+        "metaphor_set" => Set([("蝶", "踊り子")]),
+        "assoc_file" => "tint_prj/data/three_metaphor_assoc_data.csv",
+        "image_file" => "tint_prj/data/three_metaphor_images.csv",
+        "out_dir" => "tint_prj/out/",
+        "NN" => 0,
+        "steps" => 10,
+        "search_method" => "deterministic",
+        "seed" => 1234,
+        "verbose" => true,
     )
-    # config = TINT.ObjectCfg(config)
+    config = TINT.ObjectCfg(config)
+    TINT.main(config)
+end
+
+function run_triangle()
+    config = Dict{String, Any}(
+        "metaphor_set" => Set([("蝶", "踊り子")]),
+        "assoc_file" => "tint_prj/data/three_metaphor_assoc_data.csv",
+        "image_file" => "tint_prj/data/three_metaphor_images.csv",
+        "out_dir" => "tint_prj/out/",
+        "NN" => 0,
+        "steps" => 10,
+        "search_method" => "deterministic",
+        "seed" => 1234,
+        "verbose" => true,
+    )
     config = TINT.TriangleCfg(config)
+    TINT.main(config)
+end
 
-    metaphor_history, F_history = TINT.main(config)
-
-    return metaphor_history, F_history
+function main(;mode::String = "object")
+    if mode == "object"
+        run_object()
+    elseif mode == "triangle"
+        run_triangle()
+    elseif mode == "all"
+        run_object()
+        run_triangle()
+    else
+        throw(DomainError(mode, "Invalid mode selected."))
+    end
+    println("Completed.")
 end
